7c659abfd7b18269ce549997ee492cf5b270e1ed
Igor Fedorov <bullet-1@yandex.ru>
2025-08-22 20:26:29 +0500

    KS-2-4 refactor: variable "buff" changed

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,8 +3,8 @@ package org.example.lesson_2
 fun main() {
     val crystalOreAmount = 7
     val ironOreAmount = 11
-    val buffMultiplier = 0.2
+    val percentageBuff = 20
 
-    println("Bonus crystal ore: ${(crystalOreAmount * buffMultiplier).toInt()}")
-    println("Bonus iron ore: ${(ironOreAmount * buffMultiplier).toInt()}")
+    println("Bonus crystal ore: ${(crystalOreAmount * percentageBuff / 100)}")
+    println("Bonus iron ore: ${(ironOreAmount * percentageBuff / 100)}")
 }
