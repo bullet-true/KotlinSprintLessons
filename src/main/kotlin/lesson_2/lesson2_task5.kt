c6296bbd73f61702ecf2db0586bc9634a8812905
Igor Fedorov <bullet-1@yandex.ru>
2025-08-22 21:37:48 +0500

    KS-2-5 refactor: removed unnecessary variables, fixed formatting

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,18 +1,14 @@
 package org.example.lesson_2
 
+import java.util.Locale
 import kotlin.math.pow
-
-const val DAYS_IN_YEAR = 365
 
 fun main() {
     val initialDeposit = 70_000
     val interestRate = 16.7
     val investmentDuration = 20
-    val interestAccrualPeriodDays = 365 // период начисления процентов - в конце года
 
-    val finalAmount = initialDeposit * (1 + (interestRate * interestAccrualPeriodDays) / (DAYS_IN_YEAR * 100)).pow(
-        investmentDuration
-    )
-    val formatedFinalAmount = "%.3f".format(finalAmount).replace(',', '.').toDouble()
+    val finalAmount = initialDeposit * (1 + interestRate / 100).pow(investmentDuration)
+    val formatedFinalAmount = String.format(Locale.US, "%.3f", finalAmount)
     println("Размер вклада через $investmentDuration лет:  $formatedFinalAmount руб.")
 }
