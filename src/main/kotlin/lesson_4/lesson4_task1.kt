f7bae31c98571ceee40d53e448bab9d51ac72183
Igor Fedorov <bullet-1@yandex.ru>
2025-08-23 14:31:41 +0500

    KS-4-1 checking for available tables

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_4
+
+const val TOTAL_TABLES = 13
+
+fun main() {
+    val reservedToday = 13
+    val reservedTomorrow = 9
+
+    println("Доступность столиков на сегодня: ${reservedToday < TOTAL_TABLES}")
+    println("Доступность столиков на завтра: ${reservedTomorrow < TOTAL_TABLES}")
+}
