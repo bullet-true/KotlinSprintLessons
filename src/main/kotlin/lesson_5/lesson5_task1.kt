084d4e70db084601019ea908fe14b25287f6577d
Igor Fedorov <bullet-1@yandex.ru>
2025-08-25 21:25:37 +0500

    KS-5-1 refactor: rename variables

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,14 +3,14 @@ package org.example.lesson_5
 import kotlin.random.Random
 
 fun main() {
-    val a = Random.nextInt(1, 101)
-    val b = Random.nextInt(1, 101)
-    println("Проверка на бота. Вычислите результат сложения $a и $b")
+    val firstRandomNumber = Random.nextInt(1, 101)
+    val secondRandomNumber = Random.nextInt(1, 101)
+    println("Проверка на бота. Вычислите результат сложения $firstRandomNumber и $secondRandomNumber")
 
     val userInput = readln().toIntOrNull()
     val result = when (userInput) {
         null -> "Введено не число!"
-        a + b -> "Добро пожаловать!"
+        firstRandomNumber + secondRandomNumber -> "Добро пожаловать!"
         else -> "Доступ запрещен."
     }
     println(result)
