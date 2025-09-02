41bbe5ae2df80006c5ea1bfe3461a88548093280
Igor Fedorov <bullet-1@yandex.ru>
2025-09-02 23:51:18 +0500

    KS-6-2 deleted loop from user timer

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -2,18 +2,11 @@ package org.example.lesson_6
 
 fun main() {
     println("На сколько секунд установить таймер?:")
-
     val userInput = readln().toIntOrNull()?.takeIf { it > 0 } ?: run {
         println("Вы ввели не положительное число секунд!")
         return
     }
 
-    var counter = userInput
-    while (counter > 0) {
-        println("Осталось $counter сек")
-        Thread.sleep(1000)
-        counter--
-    }
-
+    Thread.sleep(userInput * 1000L)
     println("Прошло $userInput секунд")
 }
