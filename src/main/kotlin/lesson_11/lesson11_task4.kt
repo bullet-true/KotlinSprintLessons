711b475be1783e1e593bf1068dbcccb6ed64b2ec
Igor Fedorov <bullet-1@yandex.ru>
2025-09-08 09:54:25 +0500

    KS-11-4 refactor: change properties in RecipeCategory, Recipe, Ingredient

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -3,29 +3,23 @@ package org.example.lesson_11
 data class RecipeCategory(
     val id: Int,
     val name: String,
-    val image: String,
+    val imageUrl: String,
     val description: String,
+    val listOfRecipes: List<Recipe>,
 )
 
 data class Recipe(
     val id: Int,
     val name: String,
-    val image: String,
-    val servings: Int,
-    val ingredients: MutableList<Ingredient>,
-    val cookingMethod: String,
+    val imageUrl: String,
+    val ingredients: List<Ingredient>,
+    val cookingMethod: List<String>,
     val isFavorite: Boolean = false,
 )
 
 data class Ingredient(
     val id: Int,
     val name: String,
-    val amount: Int,
-    val unit: UnitType,
+    val amount: String,
+    val unit: String,
 )
-
-enum class UnitType {
-    GRAM,
-    PIECE,
-    TABLESPOON,
-}
