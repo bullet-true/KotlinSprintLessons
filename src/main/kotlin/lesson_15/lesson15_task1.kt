bb37d148de509f178972631056fc00565f227bd1
Igor Fedorov <bullet-1@yandex.ru>
2025-09-11 17:35:16 +0500

    KS-15-1 add interfaces for flyable and swimmable creatures

---
+++ src/main/kotlin/lesson_15/lesson15_task1.kt
@@ -0,0 +1,45 @@
+package org.example.lesson_15
+
+fun main() {
+    val crucian = Crucian()
+    crucian.swim()
+
+    val seagull = Seagull()
+    seagull.fly()
+
+    val duck = Duck()
+    duck.apply {
+        swim()
+        fly()
+    }
+}
+
+interface Flyable {
+    fun fly()
+}
+
+interface Swimmable {
+    fun swim()
+}
+
+class Crucian() : Swimmable {
+    override fun swim() {
+        println("Карась плывет")
+    }
+}
+
+class Seagull() : Flyable {
+    override fun fly() {
+        println("Чайка летит")
+    }
+}
+
+class Duck() : Swimmable, Flyable {
+    override fun swim() {
+        println("Утка плывет")
+    }
+
+    override fun fly() {
+        println("Утка летит")
+    }
+}
