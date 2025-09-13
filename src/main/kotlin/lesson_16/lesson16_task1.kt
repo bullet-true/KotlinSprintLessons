456233fe91519d5823c4ff188a8d4135a2690be1
Igor Fedorov <bullet-1@yandex.ru>
2025-09-13 16:22:21 +0500

    KS-16-1 refactor: use extension function for randomizing

--- src/main/kotlin/lesson_16/lesson16_task1.kt
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -1,14 +1,12 @@
 package org.example.lesson_16
 
-import kotlin.random.Random
-
 fun main() {
     val dice = Dice()
     dice.printNumber()
 }
 
 class Dice {
-    private val number: Int = Random.nextInt(1, 7)
+    private val number: Int = (1..6).random()
 
     fun printNumber() {
         println("$number")
