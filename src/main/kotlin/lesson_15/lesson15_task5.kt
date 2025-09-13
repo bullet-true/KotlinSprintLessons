a44992c99c19d2de48b6ea81ad64ea0dbdfbc4e8
Igor Fedorov <bullet-1@yandex.ru>
2025-09-13 21:32:01 +0500

    KS-15-5 refactor: eliminate code duplication

--- src/main/kotlin/lesson_15/lesson15_task5.kt
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -23,16 +23,59 @@ interface PassengerTransport {
     val maxPassengers: Int
     var currentPassengers: Int
 
-    fun loadPassengers(personCount: Int)
-    fun unloadPassengers(personCount: Int)
+    private val freeSeats get() = maxPassengers - currentPassengers
+
+    fun loadPassengers(personCount: Int) {
+
+        if (freeSeats == 0) {
+            println("В транспорте нет мест для посадки!")
+            return
+        }
+
+        val passengerLoaded = personCount.coerceAtMost(freeSeats)
+        currentPassengers += passengerLoaded
+        println("$passengerLoaded пассажир(а) сел(и) в транспорт. Осталось свободных мест: $freeSeats")
+    }
+
+    fun unloadPassengers(personCount: Int) {
+        if (currentPassengers == 0) {
+            println("В транспорте нет пассажиров!")
+            return
+        }
+
+        val passengerUnloaded = personCount.coerceAtMost(currentPassengers)
+        currentPassengers -= passengerUnloaded
+        println("$passengerUnloaded пассажир(а) вышел(ли) из транспорта. Осталось пассажиров: ${currentPassengers}. Осталось свободных мест: $freeSeats")
+    }
 }
 
 interface CargoTransport {
     val maxCargo: Int
     var currentCargo: Int
 
-    fun loadCargo(weight: Int)
-    fun unloadCargo(weight: Int)
+    private val freeSpace get() = maxCargo - currentCargo
+
+    fun loadCargo(weight: Int) {
+        if (freeSpace == 0) {
+            println("Грузовик полностью загружен, загрузить груз невозможно!")
+            return
+        }
+
+        val cargoLoaded = weight.coerceAtMost(freeSpace)
+        currentCargo += cargoLoaded
+        println("Загружено $cargoLoaded тонна(ы) в грузовик. Можно еще загрузить: $freeSpace тонн(ы)")
+    }
+
+    fun unloadCargo(weight: Int) {
+        if (currentCargo == 0) {
+            println("Грузовик пустой, разгрузка невозможна!")
+            return
+        }
+
+        val cargoUploaded = weight.coerceAtMost(currentCargo)
+        currentCargo -= cargoUploaded
+        println("Выгружено $cargoUploaded тонн(ы). Осталось в грузовике: $currentCargo тонн(ы)")
+    }
 }
 
 abstract class Car(val model: String) : Vehicle {
@@ -51,28 +94,14 @@ class PassengerCar(
     override var currentPassengers: Int = 0
 ) : Car(model), PassengerTransport {
 
-    private val freeSeats get() = maxPassengers - currentPassengers
-
     override fun loadPassengers(personCount: Int) {
-        if (freeSeats == 0) {
-            println("В транспорте \"$model\" нет мест для посадки!")
-            return
-        }
-
-        val passengerLoaded = personCount.coerceAtMost(freeSeats)
-        currentPassengers += passengerLoaded
-        println("$passengerLoaded пассажир(а) сел(и) в транспорт \"$model\". Осталось свободных мест: $freeSeats")
+        print("Загрузка пассажиров в $model: ")
+        super.loadPassengers(personCount)
     }
 
     override fun unloadPassengers(personCount: Int) {
-        if (currentPassengers == 0) {
-            println("В транспорте \"$model\" нет пассажиров!")
-            return
-        }
-
-        val passengerUnloaded = personCount.coerceAtMost(currentPassengers)
-        currentPassengers -= passengerUnloaded
-        println("$passengerUnloaded пассажир(а) вышел(ли) из транспорта \"$model\". Осталось пассажиров: ${currentPassengers}. Осталось свободных мест: $freeSeats")
+        print("Выгрузка пассажиров из $model: ")
+        super.unloadPassengers(personCount)
     }
 }
 
@@ -84,50 +113,23 @@ class Truck(
     override var currentPassengers: Int = 0
 ) : Car(model), PassengerTransport, CargoTransport {
 
-    private val freeSeats get() = maxPassengers - currentPassengers
-    private val freeSpace get() = maxCargo - currentCargo
-
-    override fun loadCargo(weight: Int) {
-        if (freeSpace == 0) {
-            println("Грузовик \"$model\" полностью загружен, загрузить груз невозможно!")
-            return
-        }
-
-        val cargoLoaded = weight.coerceAtMost(freeSpace)
-        currentCargo += cargoLoaded
-        println("Загружено $cargoLoaded тонна(ы) в грузовик \"$model\". Можно еще загрузить: $freeSpace тонн(ы)")
-    }
-
-    override fun unloadCargo(weight: Int) {
-        if (currentCargo == 0) {
-            println("Грузовик \"$model\" пустой, разгрузка невозможна!")
-            return
-        }
-
-        val cargoUploaded = weight.coerceAtMost(currentCargo)
-        currentCargo -= cargoUploaded
-        println("Из \"$model\" выгружено $cargoUploaded тонн(ы). Осталось в грузовике: $currentCargo тонн(ы)")
-    }
-
     override fun loadPassengers(personCount: Int) {
-        if (freeSeats == 0) {
-            println("В транспорте \"$model\" нет мест для посадки!")
-            return
-        }
-
-        val passengerLoaded = personCount.coerceAtMost(freeSeats)
-        currentPassengers += passengerLoaded
-        println("$passengerLoaded пассажир(а) сел(и) в транспорт \"$model\". Осталось свободных мест: $freeSeats")
+        print("Загрузка пассажиров в $model: ")
+        super.loadPassengers(personCount)
     }
 
     override fun unloadPassengers(personCount: Int) {
-        if (currentPassengers == 0) {
-            println("В транспорте \"$model\" нет пассажиров!")
-            return
-        }
+        print("Выгрузка пассажиров из $model: ")
+        super.unloadPassengers(personCount)
+    }
 
-        val passengerUnloaded = personCount.coerceAtMost(currentPassengers)
-        currentPassengers -= passengerUnloaded
-        println("$passengerUnloaded пассажир(а) вышел(ли) из транспорта \"$model\". Осталось пассажиров: ${currentPassengers}. Осталось свободных мест: $freeSeats")
+    override fun loadCargo(weight: Int) {
+        print("Загрузка груза в $model: ")
+        super.loadCargo(weight)
+    }
+
+    override fun unloadCargo(weight: Int) {
+        print("Разгрузка груза из $model: ")
+        super.unloadCargo(weight)
     }
 }
