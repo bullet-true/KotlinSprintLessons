b94a2cba1491d629205ef53e72f11f33cf7ebfd1
Igor Fedorov <bullet-1@yandex.ru>
2025-09-15 09:05:35 +0500

    KS-16-4 refactor: add manager request method for changing order status

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -3,7 +3,13 @@ package org.example.lesson_16
 class Order(private val orderNumber: Int) {
     private var isOrderReady: Boolean = false
 
-    fun updateOrderStatus(newStatus: Boolean) {
+    private fun updateOrderStatus(newStatus: Boolean) {
         isOrderReady = newStatus
+        println("Статус заказа №$orderNumber изменена на: ${if (isOrderReady) "готов" else "не готов"}")
+    }
+
+    fun requestStatusChange(newStatus: Boolean) {
+        println("Заявка менеджеру на изменение статуса заказа №$orderNumber")
+        updateOrderStatus(newStatus)
     }
 }
