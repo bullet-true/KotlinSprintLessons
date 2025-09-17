c00e215c0c3845a92cf1490436057d1f735d222c
Igor Fedorov <bullet-1@yandex.ru>
2025-09-17 12:15:00 +0500

    KS-18-5 compete Screen class with draw methods

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -1,7 +1,19 @@
 package org.example.lesson_18
 
 fun main() {
+    val screen = Screen()
 
+    val circle = Circle(2)
+    val square = Square(5.5)
+    val point = Point()
+
+    println(screen.draw(3, 8, circle))
+    println(screen.draw(4, 10, square))
+    println(screen.draw(6, 11, point))
+
+    println(screen.draw(3.3f, 8.5f, circle))
+    println(screen.draw(4.6f, 10.8f, square))
+    println(screen.draw(6.1f, 11.4f, point))
 }
 
 abstract class Figure
@@ -14,15 +26,20 @@ class Point : Figure()
 
 class Screen {
     fun draw(x: Int, y: Int, figure: Figure): String {
-        return when(figure) {
-            is Circle -> ""
-            is Square -> ""
-            is Point -> ""
-            else -> ""
+        return when (figure) {
+            is Circle -> "Нарисован круг с радиусом ${figure.radius} в координатах ($x, $y)"
+            is Square -> "Нарисован квадрат со стороной ${figure.side} в координатах ($x, $y)"
+            is Point -> "Нарисована точка в координатах ($x, $y)"
+            else -> "Нарисована неизвестная фигура в координатах ($x, $y)"
         }
     }
 
     fun draw(x: Float, y: Float, figure: Figure): String {
-        return ""
+        return when (figure) {
+            is Circle -> "Нарисован круг с радиусом ${figure.radius} в координатах ($x, $y)"
+            is Square -> "Нарисован квадрат со стороной ${figure.side} в координатах ($x, $y)"
+            is Point -> "Нарисована точка в координатах ($x, $y)"
+            else -> "Нарисована неизвестная фигура в координатах ($x, $y)"
+        }
     }
 }
