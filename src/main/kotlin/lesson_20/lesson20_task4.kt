273e404d374a367b48904ed1205fe02901f28a21
Igor Fedorov <bullet-1@yandex.ru>
2025-09-19 10:14:38 +0500

    KS-20-4 refactor: create lambdas for list items and invoke even ones

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,9 +3,11 @@ package org.example.lesson_20
 fun main() {
     val listOfStrings = listOf("First", "Second", "Third", "Fourth", "Fifth", "Sixth")
 
-    val transformToLambdas: (List<String>) -> (List<Unit>) = { list ->
-        list.map { string -> println("Нажат элемент [$string]") }
+    val transformToLambdas: (List<String>) -> (List<() -> Unit>) = { list ->
+        list.map { string -> { println("Нажат элемент [$string]") } }
     }
 
-    transformToLambdas(listOfStrings.filterIndexed { index, _ -> index % 2 == 0 })
+    val lambdas = transformToLambdas(listOfStrings)
+    lambdas.filterIndexed { index, _ -> index % 2 == 0 }
+        .forEach { it() }
 }
