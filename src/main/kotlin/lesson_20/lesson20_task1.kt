3742d80d9e9e3cc635d2fc8cb70205d3d7d95fbb
Igor Fedorov <bullet-1@yandex.ru>
2025-09-19 09:38:18 +0500

    KS-20-1 refactor: use username from context in lambda

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -1,6 +1,8 @@
 package org.example.lesson_20
 
 fun main() {
-    val greetings: (String) -> String = { username -> "С наступающим Новым Годом, $username!" }
-    println(greetings("Fry"))
+    val username = "Fry"
+
+    val greetings: () -> String = { "С наступающим Новым Годом, $username!" }
+    println(greetings())
 }
